d7efa79f4d00cf6a7da1ade4f9ae74b9c7e2b8f3
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2024-10-16 08:58:59 +0300

    used absolute paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "@/App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
-import { persistor, store } from "./redux/store";
+import { persistor, store } from "@/redux/store";
 import { PersistGate } from "redux-persist/lib/integration/react";
 
 const queryClient = new QueryClient({
